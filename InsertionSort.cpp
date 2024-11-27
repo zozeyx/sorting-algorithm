25fade7912e12d1231c9c569bc57db747737cd5a
zozeyx <126737048+zozeyx@users.noreply.github.com>
2024-11-27 20:54:54 +0900

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -12,13 +12,13 @@ int main() {
     std::string inputFile = "input.txt";
     std::string outputFile = "insertion_output.txt";
 
-    // Step 1: 파일에서 데이터 읽기
+    // 파일에서 데이터 읽기
     std::vector<int> numbers = readInputFile(inputFile);
 
-    // Step 2: Insertion Sort 수행
+    // Insertion Sort 수행
     insertionSort(numbers);
 
-    // Step 3: 정렬된 데이터를 파일에 저장
+    // 정렬된 데이터를 파일에 저장
     writeOutputFile(outputFile, numbers);
 
     std::cout << "" << outputFile << " 파일에 저장되었습니다.\n";
@@ -26,14 +26,12 @@ int main() {
     return 0;
 }
 
-// Insertion Sort 구현
+// Insertion Sort 
 void insertionSort(std::vector<int> &arr) {
     int n = arr.size();
     for (int i = 1; i < n; i++) {
         int currentElement = arr[i];
         int j = i - 1;
-
-        // 정렬된 부분에서 currentElement가 들어갈 위치 찾기
         while (j >= 0 && arr[j] > currentElement) {
             arr[j + 1] = arr[j];
             j--;
@@ -42,7 +40,7 @@ void insertionSort(std::vector<int> &arr) {
     }
 }
 
-// 파일에서 데이터를 읽어 벡터로 반환
+
 std::vector<int> readInputFile(const std::string &filename) {
     std::vector<int> arr;
     std::ifstream inputFile(filename);
@@ -61,7 +59,7 @@ std::vector<int> readInputFile(const std::string &filename) {
     return arr;
 }
 
-// 벡터 데이터를 파일로 저장
+
 void writeOutputFile(const std::string &filename, const std::vector<int> &arr) {
     std::ofstream outputFile(filename);
 
